bc376dad8139aa7f9126a4f90c933c8a7c16a363
lskrvzhnr <41797694+lskrvzhnr@users.noreply.github.com>
2022-10-02 22:16:12 +0600

    4.5

--- app/src/main/java/com/example/lesson4/MainActivity.kt
+++ app/src/main/java/com/example/lesson4/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lesson4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -17,13 +18,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 class Dice(val numSides: Int) {
